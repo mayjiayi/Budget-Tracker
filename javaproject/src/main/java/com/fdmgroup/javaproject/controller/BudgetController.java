5e2449732fdfe9df3b1fc14fac025b85831ef215
May Lwin <may.lwin@fdmgroup.com>
2024-03-30 12:48:46 +0800

    added budgetController and added createBudget method in budgetService class

---
+++ javaproject/src/main/java/com/fdmgroup/javaproject/controller/BudgetController.java
@@ -0,0 +1,5 @@
+package com.fdmgroup.javaproject.controller;
+
+public class BudgetController {
+
+}
--- javaproject/src/main/java/com/fdmgroup/javaproject/service/BudgetService.java
+++ javaproject/src/main/java/com/fdmgroup/javaproject/service/BudgetService.java
@@ -1,8 +1,30 @@
 package com.fdmgroup.javaproject.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fdmgroup.javaproject.model.Budget;
+import com.fdmgroup.javaproject.repository.BudgetRepository;
 
 @Service
 public class BudgetService {
+	
+	@Autowired
+	private BudgetRepository budgetRepo;
+	
+	public boolean createBudget(Budget budget) {
+		Optional<Budget> returnedBudget = budgetRepo.findById(budget.getBudgetID());
+		
+		if (returnedBudget.isEmpty()) {
+			budgetRepo.save(budget);
+			return true;
+		} else {
+			System.out.println("Budget already exists");
+			return false;
+		}
+		
+	}
 
 }
