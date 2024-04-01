270681c5017f4892deb0cb68b568d53f1a5b86a2
May Lwin <may.lwin@fdmgroup.com>
2024-04-01 22:20:34 +0800

    added logging and javadocs into CategoryController class

--- javaproject/src/main/java/com/fdmgroup/javaproject/controller/CategoryController.java
+++ javaproject/src/main/java/com/fdmgroup/javaproject/controller/CategoryController.java
@@ -3,20 +3,26 @@ package com.fdmgroup.javaproject.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.fdmgroup.javaproject.model.Category;
-import com.fdmgroup.javaproject.repository.CategoryRepository;
 import com.fdmgroup.javaproject.service.CategoryService;
 
 import jakarta.annotation.PostConstruct;
 
+/**
+ * CategoryController is responsible for initializing a list of pre-set categories for ease of data compilation
+ * 
+ */
 @Controller
 public class CategoryController {
 	
 	@Autowired
 	private CategoryService categoryService;
+	private static final Logger logger = LoggerFactory.getLogger(Category.class);
 	
 	@PostConstruct
 	public void init() {
@@ -33,5 +39,6 @@ public class CategoryController {
 		for(String category : categories) {
 			categoryService.createCategory(category);
 		}
+		logger.info("All categories present in database.");
     }
 }
