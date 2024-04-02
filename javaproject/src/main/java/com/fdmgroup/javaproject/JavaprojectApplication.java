f839783078fe167276aa1aba89c780f32042fcb3
May Lwin <may.lwin@fdmgroup.com>
2024-04-02 23:45:04 +0800

    added css to the index, login and register htmls

--- javaproject/src/main/java/com/fdmgroup/javaproject/JavaprojectApplication.java
+++ javaproject/src/main/java/com/fdmgroup/javaproject/JavaprojectApplication.java
@@ -9,5 +9,4 @@ public class JavaprojectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaprojectApplication.class, args);
 	}
-
 }
--- javaproject/src/main/java/com/fdmgroup/javaproject/controller/UserController.java
+++ javaproject/src/main/java/com/fdmgroup/javaproject/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.fdmgroup.javaproject.controller;
 
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,80 +9,73 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import com.fdmgroup.javaproject.model.User;
 import com.fdmgroup.javaproject.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
 /**
- * UserController is responsible for returning html templates for anything user entity related 
+ * UserController is responsible for returning html templates for anything user
+ * entity related
  */
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
 	private static final Logger logger = LoggerFactory.getLogger(User.class);
-	
-	@GetMapping("/") 
+
+	@GetMapping("/")
 	public String slash() {
-		return ("register");
+		return ("index");
 	}
-	
-	@GetMapping("/register") 
+
+	@GetMapping("/register")
 	public String register() {
 		return ("register");
 	}
-	
+
 	@PostMapping("/register")
-	public String processRegistration(	@RequestParam String username, 
-										@RequestParam String password, 
-										@RequestParam String email,
-										@RequestParam String firstName, 
-										@RequestParam String lastName
-										) {
+	public String processRegistration(@RequestParam String username, @RequestParam String password,
+			@RequestParam String email, @RequestParam String firstName, @RequestParam String lastName) {
 		logger.info("Registering User '" + username + "' ...");
 		logger.info("Username : " + username + " | Password : " + password + " | Email : " + email);
-		
+
 		User newUser = new User(username, password, email, firstName, lastName);
 		if (userService.registerNewUser(newUser)) {
 			logger.info("User '" + username + "' registered.");
-			return("redirect:/login");
+			return ("redirect:/login");
 		} else {
 			logger.warn("Unable to register user '" + username + "'.");
-			return("register");
+			return ("register");
 		}
 	}
-	
-	@GetMapping("/login") 
+
+	@GetMapping("/login")
 	public String login() {
 		return ("login");
 	}
-	
+
 	@PostMapping("/login")
-	public String processLogin(	@RequestParam String username, 
-            					@RequestParam String password, 
-            					HttpSession session) {
+	public String processLogin(@RequestParam String username, @RequestParam String password, HttpSession session) {
 		Optional<User> targetUser = userService.validatePassword(username, password);
-    	if (targetUser.isEmpty()) {
-    		logger.warn("Authentication failed for User '" + username + "'.");
-    		return "login";
-    	} else {
-    		logger.info("Authentication was successful for User '" + username + "'.");
-    		
-    		User foundUser = userService.findUser(username);
-    		int currentUserId = foundUser.getUserID();
-    		
-    		session.setAttribute("user", foundUser);
-    		session.setMaxInactiveInterval(1800);
-    		
-    		logger.info("Current User " + username + " added to session attribute.");
-    		
-    		return "redirect:/dashboard/" + currentUserId;
-    	}
+		if (targetUser.isEmpty()) {
+			logger.warn("Authentication failed for User '" + username + "'.");
+			return "login";
+		} else {
+			logger.info("Authentication was successful for User '" + username + "'.");
+
+			User foundUser = userService.findUser(username);
+			int currentUserId = foundUser.getUserID();
+
+			session.setAttribute("user", foundUser);
+			session.setMaxInactiveInterval(1800);
+
+			logger.info("Current User " + username + " added to session attribute.");
+
+			return "redirect:/dashboard/" + currentUserId;
+		}
 	}
-	
-	
-		
 
 }
